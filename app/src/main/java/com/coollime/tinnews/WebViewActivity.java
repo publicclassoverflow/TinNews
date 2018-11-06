6d90a013b2c60403679c5c20a94ef3604fd1a4d1
meng <mz.zhoumeng@gmail.com>
2018-11-06 04:01:41 -0800

    Added a Google page to play with activities switching

--- app/src/main/java/com/coollime/tinnews/WebViewActivity.java
+++ app/src/main/java/com/coollime/tinnews/WebViewActivity.java
@@ -1,13 +1,65 @@
 package com.coollime.tinnews;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class WebViewActivity extends AppCompatActivity {
+    public static final String URL = "url";
+    private String url;
+    private ProgressBar progressBar;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+        findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        progressBar = findViewById(R.id.progress_bar);
+        final WebView webView = findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                webView.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                // TODO Auto-generated method stub
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+//        Bundle bundle = getIntent().getExtras();
+//        if (bundle != null) {
+//            Bundle innerBundle = bundle.getBundle(BUNDLE);
+//            if (innerBundle != null) {
+//                url = innerBundle.getString(URL);
+//                webView.loadUrl(url);
+//            }
+//        }
+        url = "https://www.google.com";
+        webView.loadUrl(url);
+
+        findViewById(R.id.more).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//            showMenu(v);
+            }
+        });
     }
 }
