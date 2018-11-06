9d662152cb296ffa01d86948ec7925dc04879d2c
meng <mz.zhoumeng@gmail.com>
2018-11-06 03:52:52 -0800

    MainActivity added

---
+++ app/src/main/java/com/coollime/tinnews/MainActivity.java
@@ -0,0 +1,24 @@
+package com.coollime.tinnews;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView view = findViewById(R.id.text);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
