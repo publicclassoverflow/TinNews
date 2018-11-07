1c4f405e04de03ca92475f4ba8455d88cb1c4be0
meng <mz.zhoumeng@gmail.com>
2018-11-07 03:35:07 -0800

    Getting US top headlines from newsapi.org

--- app/src/main/java/com/coollime/tinnews/tin/TinGalleryFragment.java
+++ app/src/main/java/com/coollime/tinnews/tin/TinGalleryFragment.java
@@ -9,9 +9,16 @@ import android.view.ViewGroup;
 
 import com.coollime.tinnews.R;
 import com.coollime.tinnews.common.TinBasicFragment;
+import com.coollime.tinnews.retrofit.NewsRequestApi;
+import com.coollime.tinnews.retrofit.RetrofitClient;
 import com.coollime.tinnews.retrofit.response.News;
 import com.mindorks.placeholderview.SwipeDecor;
 import com.mindorks.placeholderview.SwipePlaceHolderView;
+
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,9 +26,7 @@ import com.mindorks.placeholderview.SwipePlaceHolderView;
 public class TinGalleryFragment extends TinBasicFragment implements TinNewsCard.OnSwipeListener {
     private SwipePlaceHolderView mSwipeView;
 
-
     public static TinGalleryFragment newInstance() {
-
         Bundle args = new Bundle();
 
         TinGalleryFragment fragment = new TinGalleryFragment();
@@ -29,15 +34,12 @@ public class TinGalleryFragment extends TinBasicFragment implements TinNewsCard.
         return fragment;
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_tin_gallery, container, false);
-
         mSwipeView = view.findViewById(R.id.swipeView);
-
         mSwipeView.getBuilder()
                 .setDisplayViewCount(3)
                 .setSwipeDecor(new SwipeDecor()
@@ -45,32 +47,35 @@ public class TinGalleryFragment extends TinBasicFragment implements TinNewsCard.
                         .setRelativeScale(0.01f)
                         .setSwipeInMsgLayoutId(R.layout.tin_news_swipe_in_msg_view)
                         .setSwipeOutMsgLayoutId(R.layout.tin_news_swipe_out_msg_view));
-
         view.findViewById(R.id.rejectBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mSwipeView.doSwipe(false);
             }
         });
-
         view.findViewById(R.id.acceptBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mSwipeView.doSwipe(true);
             }
         });
+        getData();
+        return view;
+    }
 
-        // fake data
-        for (int i = 0; i < 10; i++) {
-            News news = new News();
-            news.image = "https://i.ytimg.com/vi/BgIJ45HKDpw/maxresdefault.jpg";
+    private void getData() {
+        RetrofitClient.getInstance().create(NewsRequestApi.class).getNewsByCountry("us")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .filter(baseResponse -> baseResponse != null && baseResponse.articles != null)
+                .subscribe(baseResponse -> showNewsCard(baseResponse.articles));
+    }
+
+    private void showNewsCard(List<News> newsList) {
+        for (News news : newsList) {
             TinNewsCard tinNewsCard = new TinNewsCard(news, mSwipeView, this);
             mSwipeView.addView(tinNewsCard);
         }
-
-
-        return view;
-
     }
 
     @Override
